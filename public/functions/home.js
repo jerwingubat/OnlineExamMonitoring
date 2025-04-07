cc7d55491cba30e375e57edf63dc406f4159a978
jerwingubat <you@example.com>
2025-04-08 03:05:26 +0800

    fix firebase.json routes

--- public/functions/home.js
+++ public/functions/home.js
@@ -84,7 +84,7 @@ async function handleSuspension(userId, suspendedUntil, suspensionData) {
 }
 
 function redirectToLogin() {
-    window.location.href = '/public/index.html';
+    window.location.href = '/index.html';
 }
 
 function displayUserInfo(userId) {
